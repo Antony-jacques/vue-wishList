3a2bc9755d9b47c151cf2e524e66ee11a40e61e0
Antony <antony.jacques@laposte.net>
2023-11-08 08:13:15 +0100

    init project

---
+++ src/components/WishList.vue
@@ -0,0 +1,16 @@
+<template>
+    WishList
+</template>
+
+<script>
+export default {
+    props: [ 'fakeData' ],
+    setup(props){
+        console.log(props.fakeData)
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>HomeView</h1>
+  <WishList :fakeData="fakeData"/>
+</template>
+
+<script>
+import WishList from '../components/WishList.vue';
+export default {
+  setup() {
+    const fakeData = [
+      { 
+        title: 'toy',
+        price: 20
+      },
+      { 
+        title: 'clothes',
+        price: 50
+      },
+    ]
+
+    return { fakeData }
+
+  },
+  components: { WishList,  }
+}
+</script>
+
+<style>
+
+</style>
