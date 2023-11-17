f681c8f0fd6b2d09bf07fd7ed5b23b9b09c50718
Antony <antony.jacques@laposte.net>
2023-11-17 07:52:47 +0100

    display wishes

--- src/components/WishList.vue
+++ src/components/WishList.vue
@@ -1,28 +1,53 @@
 <template>
-    <div>
-        <div class="wishItem" v-for="item in fakeData " :key="item.title">
-            <div class="item-title">
-                <h3>{{ item.title }}</h3>
-            </div>
-            <div>
-                {{ item.description }}
-            </div>
-            <div>
-                {{ item.price }}
-            </div>
-        </div>
+  <div>
+    <p>{{ error }}</p>
+
+    <div class="wishItem" v-for="item in wishList" :key="item.title">
+      <div class="item-title">
+        <h3>{{ item.title }}</h3>
+      </div>
+      <div>
+        {{ item.description }}
+      </div>
+      <div>
+        {{ item.price }}
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import { projectFirestore } from '../firebase/config'
+
 export default {
-    props: [ 'fakeData' ],
-    setup(props){
-        console.log(props.fakeData)
+  setup() {
+    const wishList = ref([])
+
+    const error = ref(null)
+
+    const load = async () => {
+      try {
+        const res = await projectFirestore.collection('wishList').get()
+
+        if (!res.docs.length) {
+          throw Error('Please, make a wish !')
+        }
+
+        wishList.value = res.docs.map((doc) => {
+          return { ...doc.data(), id: doc.id }
+        })
+      } catch (err) {
+        error.value = err.message
+      }
     }
+
+    load()
+
+    return { wishList, error }
+  }
 }
 </script>
 
 <style>
-
 </style>
