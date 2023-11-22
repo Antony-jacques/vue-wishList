f34973fe1ec5816781cc30d29a691c9647334198
Antony JACQUES <antony.jacques-ext@gl-events.com>
2023-11-22 08:28:09 +0100

    clean

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,15 @@
 <template>
   <h1>HomeView</h1>
-  <WishList :fakeData="fakeData" />
+  <WishList  />
 </template>
 
 <script>
 import WishList from '../components/WishList.vue'
 export default {
   setup() {
-    const fakeData = [
-      {
-        title: 'toy',
-        price: 20,
-        description:
-          '  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque eveniet, consectetur dolorum nesciunt minus eos amet, ipsum alias sit iusto, aliquid id veniam molestias sint ea sed quam sapiente voluptate maxime pariatur quas deserunt molestiae. Sint, quam. Quas, repellat! Natus harum voluptatum sequi qui ullam, sit omnis nemo maiores eveniet!'
-      },
-      {
-        title: 'clothes',
-        price: 50,
-        description:
-          '  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque eveniet, consectetur dolorum nesciunt minus eos amet, ipsum alias sit iusto, aliquid id veniam molestias sint ea sed quam sapiente voluptate maxime pariatur quas deserunt molestiae. Sint, quam. Quas, repellat! Natus harum voluptatum sequi qui ullam, sit omnis nemo maiores eveniet!'
-      }
-    ]
 
-    return { fakeData }
+
+    return {  }
   },
   components: { WishList }
 }
