0e3e848ad1688eb157fe097d45958f21f64ad926
Antony JACQUES <antony.jacques-ext@gl-events.com>
2023-12-04 21:28:45 +0100

    add wishes

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home-header">
+    <div class="title">
+      <h2>Ma liste au Père Noel</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.home-header {
+  display: flex;
+  height: 75vh;
+  background: url('../assets/christmas6.webp');
+  background-size: cover;
+  -moz-background-size: cover;
+  -webkit-background-size: cover;
+  -o-background-size: cover;
+  background-position: center center;
+  position: relative;
+}
+
+.home-header::after {
+  background: url('../assets/snow.webp') 100% 100%;
+  /* background: red; */
+  height: 147px;
+  position: absolute;
+  display: block;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  content: '';
+}
+
+.title {
+  margin: auto;
+  transform: translateY(-100%);
+  background: white;
+  padding: 1rem;
+  border-radius: 50px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,25 @@
 <template>
-  <h1>HomeView</h1>
-  <WishList  />
+  <div class="container">
+    <HomeHeader />
+    <WishList />
+
+  </div>
 </template>
 
 <script>
 import WishList from '../components/WishList.vue'
+import HomeHeader from '../components/HomeHeader.vue'
+
 export default {
   setup() {
-
-
-    return {  }
+    return {}
   },
-  components: { WishList }
+  components: { WishList, HomeHeader }
 }
 </script>
 
 <style>
+  .container {
+    width: 100%
+  }
 </style>
